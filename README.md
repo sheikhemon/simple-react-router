76d9facfb54ec02b4a804e306330cf226f4223cb
Emon Sheikh <sheikhemon@gmail.com>
2021-10-16 17:39:45 +0600

    router-setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # React Router Practice
+
+# 5 core steps react router
+1. BrowseRouter
+2. Switch
+3. Router
+4. path 
+5. component path
+6. exact - hidden slash
--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import Home from './Components/Home/Home';
 import Friends from './Components/Friends/Friends';
 import About from './Components/About/About';
 import NotFound from './Components/NotFound/NotFound';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
-      <NotFound></NotFound>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Home">
+            <Home></Home>
+          </Route>
+          <Route path="/Friends">
+            <Friends></Friends>
+          </Route>
+          <Route path="/About">
+            <About></About>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
