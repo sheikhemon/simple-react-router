7b06a2a0ec89a32dc6a973b1d8137b18110461e3
Emon Sheikh <sheikhemon@gmail.com>
2021-10-16 17:26:53 +0600

    create-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Friends from './Components/Friends/Friends';
+import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-
+      <Home></Home>
+      <Friends></Friends>
+      <About></About>
+      <NotFound></NotFound>
     </div>
   );
 }
---
+++ src/Components/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h3>This is friends page</h3>
+        </div>
+    );
+};
+
+export default Friends;
